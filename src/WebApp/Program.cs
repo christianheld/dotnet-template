b24c04d0d081847bb300c63da974f7cf290560aa
Christian Held <christian.held@gmx.net>
2022-08-09 21:38:09 +0200

    Add sample WebApp

---
+++ src/WebApp/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+#pragma warning disable CA1050 // false positive
+public partial class Program
+{
+}
+#pragma warning restore CA1050
---
+++ tests/WebApp.Tests/IntegrationTest.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApp.Tests;
+
+public class IntegrationTest
+{
+    [Fact]
+    public async Task ReturnsHelloWorld()
+    {
+        // Arrange
+        using var webapp = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // Override services here
+                });
+            });
+
+        using var client = webapp.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("");
+        var stringContent = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Contains("Hello World!", stringContent);
+        });
+    }
+}
