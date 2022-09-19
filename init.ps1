47f638fa11de3deab3e23400f57480e2924ab38c
Christian Held <christian.held@gmx.net>
2022-09-19 20:07:47 +0200

    Update script to create initial project

--- init.ps1
+++ init.ps1
@@ -4,7 +4,14 @@
 Param(
     # The name of the new solution
     [Parameter(Mandatory = $true)]
-    [string] $Name
+    [string] $SolutionName,
+
+    # The project type, used for dotent new
+    [Parameter(Mandatory = $true)]
+    [string] $ProjectType,
+
+    [Parameter(Mandatory = $true)]
+    [string] $ProjectName
 )
 
 
@@ -16,16 +23,30 @@ Remove-Item -Force -Recurse  .\src\WebApp
 Remove-Item -Force -Recurse  .\tests\NetProject.Tests
 Remove-Item -Force -Recurse  .\tests\WebApp.Tests
 
-$solution = "$Name.sln"
+$solution = "$SolutionName.sln"
 Rename-Item -Path ./NetProject.sln -NewName $solution
+
+mkdir ".\src\$ProjectName"
+mkdir ".\tests\$ProjectName.Tests"
+
+Push-Location ".\src\$ProjectName"
+dotnet new $ProjectType
+Pop-Location
+
+Push-Location ".\tests\$ProjectName.Tests"
+dotnet new xunit
+dotnet add reference "..\..\src\$ProjectName"
+Pop-Location
+
+Get-ChildItem -Recurse *.csproj | ForEach-Object { dotnet sln add $_ }
 
 $cakeScript = Get-Content .\build.cake
 $cakeScript = $cakeScript.Replace(
     'string solution = "NetProject.sln";', 
-    "string solution = ""$solution"";");
+    "string solution = ""$solution"".sln;");
 
 $cakeScript | Out-File "build.cake" -Encoding utf8NoBOM
 
-Remove-Item .\init.ps1
+# Remove-Item .\init.ps1
 
 Write-Output "Commit changes to git to complete initialization."
