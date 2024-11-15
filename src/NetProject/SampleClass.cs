5136eee1f04ff5fa54fcde7dce728aac375358f8
Christian Held <christian.held@gmx.net>
2024-11-15 20:09:07 +0100

    Update to .NET 9

--- src/NetProject/SampleClass.cs
+++ src/NetProject/SampleClass.cs
@@ -3,22 +3,15 @@ namespace NetProject;
 /// <summary>
 /// This is an example class.
 /// </summary>
-public class SampleClass
+/// <remarks>
+/// Initializes a new instance of the <see cref="SampleClass"/> class.
+/// </remarks>
+/// <param name="name">The name for the greeting.</param>
+internal class SampleClass(string name)
 {
-    private readonly string _name;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SampleClass"/> class.
-    /// </summary>
-    /// <param name="name">The name for the greeting.</param>
-    public SampleClass(string name)
-    {
-        _name = name;
-    }
-
     /// <summary>
     /// Gets the greeting text.
     /// </summary>
     /// <returns>The greeting text.</returns>
-    public string Greeting => $"Hello, {_name}";
+    public string Greeting => $"Hello, {name}";
 }
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -4,9 +4,3 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello, World!");
 
 app.Run();
-
-#pragma warning disable CA1050 // false positive
-public partial class Program
-{
-}
-#pragma warning restore CA1050
