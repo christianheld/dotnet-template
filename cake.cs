0efd9d941a4dc0bbe1b98052161bde8ca5f50cd2
Christian Held <christian.held@gmx.net>
2026-01-31 10:32:24 +0100

    Cleanup

--- cake.cs
+++ cake.cs
@@ -12,17 +12,18 @@ var configuration = Argument("configuration", "Release");
 
 var dotNetVerbosity = DotNetVerbosity.Minimal;
 var msBuildSettings = new DotNetMSBuildSettings()
-        .SetMaxCpuCount(0)
-        .TreatAllWarningsAs(MSBuildTreatAllWarningsAs.Error)
-        .WithArgumentCustomization(args => args.Append("-warnnotaserror:CS0618"));
+    .SetMaxCpuCount(0)
+    .TreatAllWarningsAs(MSBuildTreatAllWarningsAs.Error)
+    .WithArgumentCustomization(args => args.Append("-warnnotaserror:CS0618"));
 
-Setup(context =>
-{
-    var version = GitVersioningGetVersion();
+Setup(
+    context =>
+    {
+        var version = GitVersioningGetVersion();
 
-    context.Information($"Solution: {solution}");
-    context.Information($"Version:  {version.SemVer2}");
-});
+        context.Information($"Solution: {solution}");
+        context.Information($"Version:  {version.SemVer2}");
+    });
 
 ///////////////////////////////////////////////////////////////////////////////
 // TASKS
@@ -30,72 +31,72 @@ Setup(context =>
 
 Task("CleanArtifacts")
     .Does(() =>
-{
-    CleanDirectory("./artifacts");
-});
+    {
+        CleanDirectory("./artifacts");
+    });
 
 Task("Clean")
-   .WithCriteria(c => HasArgument("rebuild"))
-   .Does(() =>
-{
-   var objs = GetDirectories($"./**/obj");
-   var bins = GetDirectories($"./**/bin");
+    .WithCriteria(c => HasArgument("rebuild"))
+    .Does(() =>
+    {
+        var objs = GetDirectories($"./**/obj");
+        var bins = GetDirectories($"./**/bin");
 
-   CleanDirectories(objs.Concat(bins));
-});
+        CleanDirectories(objs.Concat(bins));
+    });
 
 Task("RestorePackages")
     .Does(() =>
-{
-    DotNetRestore(new DotNetRestoreSettings { Verbosity = dotNetVerbosity });
-});
+    {
+        DotNetRestore(new DotNetRestoreSettings { Verbosity = dotNetVerbosity });
+    });
 
 Task("CheckFormatting")
     .Does(() =>
-{
-    DotNetTool(
-        "format",
-        new DotNetToolSettings()
-            .WithArgumentCustomization(args => args
-                .Append("whitespace")
-                .Append("--verify-no-changes")));
-});
+    {
+        DotNetTool(
+            "format",
+            new DotNetToolSettings()
+                .WithArgumentCustomization(args => args
+                    .Append("whitespace")
+                    .Append("--verify-no-changes")));
+    });
 
 Task("Compile")
     .IsDependentOn("CheckFormatting")
     .IsDependentOn("RestorePackages")
     .Does(() =>
-{
-    DotNetBuild(
-        solution,
-        new DotNetBuildSettings
-        {
-            Configuration = configuration,
-            NoRestore = true,
-            Verbosity = dotNetVerbosity,
-            MSBuildSettings = msBuildSettings
-        });
-});
+    {
+        DotNetBuild(
+            solution,
+            new DotNetBuildSettings
+            {
+                Configuration = configuration,
+                NoRestore = true,
+                Verbosity = dotNetVerbosity,
+                MSBuildSettings = msBuildSettings
+            });
+    });
 
 Task("Test")
     .IsDependentOn("Compile")
     .Does(() =>
-{
-    DotNetTest(
-       solution,
-       new DotNetTestSettings
-       {
-          Configuration = configuration,
-          NoRestore = true,
-          NoBuild = true,
-          Verbosity = dotNetVerbosity,
-          Collectors = { "XPlat Code Coverage" }
-       }
-    );
-});
+    {
+        DotNetTest(
+            solution,
+            new DotNetTestSettings
+            {
+                Configuration = configuration,
+                NoRestore = true,
+                NoBuild = true,
+                Verbosity = dotNetVerbosity,
+                Collectors = { "XPlat Code Coverage" }
+            }
+        );
+    });
 
 Task("Default")
-   .IsDependentOn("CleanArtifacts")
-   .IsDependentOn("Test");
+    .IsDependentOn("CleanArtifacts")
+    .IsDependentOn("Test");
 
 RunTarget(target);
